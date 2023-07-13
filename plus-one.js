b2f1c0e440027b003f4eab08f72c94253e19de16
anhtt2211 <trananh.2211.work@gmail.com>
2023-07-13 16:03:25 +0700

    algo: plus-one

--- plus-one.js
+++ plus-one.js
@@ -1,22 +1,29 @@
 const plusOne = (digits) => {
-  let strNumber = "";
-  digits.forEach((num) => {
-    strNumber = strNumber + num;
-  });
+  let temp = 0;
+  let result = [];
 
-  let number = parseInt(strNumber) + 1;
-  const result = [];
-  number
-    .toString()
-    .split("")
-    .forEach((num) => {
-      result.push(parseInt(num));
-    });
+  for (let index = digits.length - 1; index >= 0; index--) {
+    const element = digits[index];
+    let number = element;
+    if (index === digits.length - 1) {
+      number = element + 1 + temp;
+    } else {
+      number += temp;
+    }
 
-  return result;
+    result.push(number % 10);
+    temp = Math.floor(number / 10);
+
+    if (index === 0 && temp !== 0) {
+      result.push(temp);
+    }
+  }
+
+  return result.reverse();
 };
 
-const digits = [6, 1, 4, 5, 3, 9, 0, 1, 9, 5, 1, 8, 6, 7, 0, 5, 5, 4, 3];
+// const digits = [6, 1, 4, 5, 3, 9, 0, 1, 9, 5, 1, 8, 6, 7, 0, 5, 5, 4, 9];
+const digits = [9, 9];
 
 const res = plusOne(digits);
 
