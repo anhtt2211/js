f5082034eaa8e7bc69f4fc7e7c69d25673e27a47
anhtt2211 <trananh.2211.work@gmail.com>
2023-08-01 15:23:25 +0700

    [easy][string] 387 First Unique Character in a String --solution-2

--- firstUniqChar.js
+++ firstUniqChar.js
@@ -2,15 +2,31 @@
  * @param {string} str
  * @return {number}
  */
+// var firstUniqChar = function (str) {
+//   const charCount = {};
+
+//   for (let index = 0; index < str.length; index++) {
+//     charCount[str[index]] = (charCount[str[index]] || 0) + 1;
+//   }
+
+//   for (let index = 0; index < str.length; index++) {
+//     if (charCount[str[index]] === 1) {
+//       return index;
+//     }
+//   }
+
+//   return -1;
+// };
+
 var firstUniqChar = function (str) {
-  const charCount = {};
+  const hasMap = new Map();
 
   for (let index = 0; index < str.length; index++) {
-    charCount[str[index]] = (charCount[str[index]] || 0) + 1;
+    hasMap.set(str[index], (hasMap.get(str[index]) || 0) + 1);
   }
 
   for (let index = 0; index < str.length; index++) {
-    if (charCount[str[index]] === 1) {
+    if (hasMap.get(str[index]) === 1) {
       return index;
     }
   }
