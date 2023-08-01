aded132550a4bbb79aff724827690ed1d5f19f74
anhtt2211 <trananh.2211.work@gmail.com>
2023-08-01 14:11:12 +0700

    [easy][string] reverseVowels WIP

---
+++ reverseVowels.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} string
+ * @return {string}
+ */
+
+// vowels are {a, e, i, o, u}
+
+var reverseVowels = function (string) {};
+
+const string1 = "hello"; // holle
+const result1 = reverseVowels(string1);
+console.log({ result1 });
+
+const string2 = "leetcode"; // leotcede
+const result2 = reverseVowels(string2);
+console.log({ result2 });
