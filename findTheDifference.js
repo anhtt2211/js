e71f433106cf342bf78af9a594231ac253a3f75b
anhtt2211 <trananh.2211.work@gmail.com>
2023-08-02 10:51:02 +0700

    [easy][string] 389 Find the Difference --solution-2

--- findTheDifference.js
+++ findTheDifference.js
@@ -3,21 +3,42 @@
  * @param {string} tStr
  * @return {character}
  */
-var findTheDifference = function (sStr, tStr) {
-  const hasMapT = new Map();
+// var findTheDifference = function (sStr, tStr) {
+//   const hasMapT = new Map();
 
-  for (let i = 0; i < tStr.length; i++) {
-    hasMapT.set(tStr[i], (hasMapT.get(tStr[i]) || 0) + 1);
+//   for (let i = 0; i < tStr.length; i++) {
+//     hasMapT.set(tStr[i], (hasMapT.get(tStr[i]) || 0) + 1);
+//   }
+
+//   for (let i = 0; i < sStr.length; i++) {
+//     const value = hasMapT.get(sStr[i]);
+//     if (value > 0) {
+//       hasMapT.set(sStr[i], value - 1);
+//     }
+//   }
+
+//   for (const [key, value] of hasMapT) {
+//     if (value === 1) {
+//       return key;
+//     }
+//   }
+// };
+
+var findTheDifference = function (sStr, tStr) {
+  const charDict = {};
+
+  for (const str of tStr) {
+    charDict[str] = (charDict[str] || 0) + 1;
   }
 
-  for (let i = 0; i < sStr.length; i++) {
-    const value = hasMapT.get(sStr[i]);
+  for (const str of sStr) {
+    const value = charDict[str];
     if (value > 0) {
-      hasMapT.set(sStr[i], value - 1);
+      charDict[str] = value - 1;
     }
   }
 
-  for (const [key, value] of hasMapT) {
+  for (const [key, value] of Object.entries(charDict)) {
     if (value === 1) {
       return key;
     }
