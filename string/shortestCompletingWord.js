bc5d423309da78c597edc3e2d80c4534e6de0156
anhtt2211 <trananh.2211.work@gmail.com>
2023-08-03 21:49:57 +0700

    [easy][string] 748. Shortest Completing Word

---
+++ string/shortestCompletingWord.js
@@ -0,0 +1,97 @@
+/**
+ * @param {string} licensePlate
+ * @param {string[]} words
+ * @return {string}
+ */
+// [easy][string] 748. Shortest Completing Word
+
+var shortestCompletingWord = function (licensePlate, words) {
+  const liscenMap = {};
+  licensePlate = licensePlate.toLowerCase();
+
+  for (let ele of licensePlate) {
+    if (/^[a-z]$/.test(ele)) {
+      liscenMap[ele] = (liscenMap[ele] || 0) + 1;
+    }
+  }
+
+  const words_valid = [];
+  for (let ele of words) {
+    const hasMap = {};
+    const liscenMap_ele = { ...liscenMap };
+
+    for (const letter of ele) {
+      hasMap[letter] = (hasMap[letter] || 0) + 1;
+    }
+
+    for (const [key, value] of Object.entries(hasMap)) {
+      const liscen_value = liscenMap_ele[key];
+
+      if (liscen_value) {
+        if (liscen_value > value) {
+          break;
+        } else {
+          liscenMap_ele[key] = liscen_value - value;
+        }
+      }
+
+      let _flag = true;
+      for (const [_key, _value] of Object.entries(liscenMap_ele)) {
+        if (_value > 0) {
+          _flag = false;
+        }
+      }
+
+      if (_flag) break;
+    }
+
+    let flag = true;
+    for (const [_key, value] of Object.entries(liscenMap_ele)) {
+      if (value > 0) {
+        flag = false;
+        break;
+      }
+    }
+    if (flag) {
+      words_valid.push(ele);
+    }
+  }
+
+  let shortest_word = words_valid[0];
+  for (let i = 1; i < words_valid.length; i++) {
+    const element = words_valid[i];
+    if (shortest_word.length > element.length) {
+      shortest_word = element;
+    }
+  }
+
+  return shortest_word;
+};
+
+console.log(
+  shortestCompletingWord("GrC8950", [
+    // "measure",
+    // "other",
+    // "every",
+    // "base",
+    "according",
+    // "level",
+    // "meeting",
+    // "none",
+    // "marriage",
+    // "rest",
+  ])
+);
+
+// console.log(
+//   shortestCompletingWord(
+//     (licensePlate = "1s3 PSt"),
+//     (words = ["step", "steps", "stripe", "stepple"])
+//   )
+// );
+// console.log(
+//   shortestCompletingWord(
+//     (licensePlate = "1s3 456"),
+//     (words = ["looks", "pest", "stew", "show"])
+//   )
+// );
