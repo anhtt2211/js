70507aee12f46cea407dc77e114a9c8628c5d8da
anhtt2211 <trananh.2211.work@gmail.com>
2023-08-14 22:01:59 +0700

    [easy][string] 409. Longest Palindrome

--- string/longestPalindrome.js
+++ string/longestPalindrome.js
@@ -2,44 +2,22 @@
  * @param {string} s
  * @return {string}
  */
+// [easy][string] 409. Longest Palindrome
 
-//  c b a b d
-//  0 1 2 3 4
 var longestPalindrome = function (string) {
-  let result = "";
-  const length = string.length - 1;
+  let result = 0;
+  const hash = {};
 
-  for (let i = 0; i <= length; i++) {
-    const j = length - i;
-    const left = string[i];
-    const right = string[j];
+  for (const char of string) {
+    hash[char] = (hash[char] || 0) + 1;
 
-    if (length === 1 && left !== right) {
-      return left;
-    }
-
-    if (left === right) {
-      result = result.concat(left);
+    if (hash[char] % 2 === 0) {
+      result += 2;
     }
   }
 
-  return result;
+  return string.length > result ? result + 1 : result;
 };
 
-// const string1 = "accd";
-// const result1 = longestPalindrome(string1); // result = cc
-// console.log({ result1 });
-
-// const string2 = "acecd";
-// const result2 = longestPalindrome(string2); // result = cec
-// console.log({ result2 });
-
-// const string3 = "ac";
-// const result3 = longestPalindrome(string3); // result = cec
-// console.log({ result3 });
-
-const string4 = "accc";
-const result4 = longestPalindrome(string4); // result = cec
-console.log({ result4 });
-
-// NOT COMPLETE
+console.log(longestPalindrome("abccccdd"));
+console.log(longestPalindrome("a"));
